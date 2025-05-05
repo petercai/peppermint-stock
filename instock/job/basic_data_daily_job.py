ebda0433cd3a321fc61669086ecc6689810cbd7c
Peter Cai <petercaica@hotmail.com>
2025-05-04 23:54:32 -0400

    instock/job/basic_data_daily_job.py

--- instock/job/basic_data_daily_job.py
+++ instock/job/basic_data_daily_job.py
@@ -8,10 +8,10 @@ import sys
 cpath_current = os.path.dirname(os.path.dirname(__file__))
 cpath = os.path.abspath(os.path.join(cpath_current, os.pardir))
 sys.path.append(cpath)
-import instock.lib.run_template as runt
-import instock.core.tablestructure as tbs
-import instock.lib.database as mdb
-import instock.core.stockfetch as stf
+import instock.lib.run_template
+import instock.core.tablestructure
+import instock.lib.database
+import instock.core.stockfetch
 from instock.core.singleton_stock import stock_data
 
 __author__ = 'myh '
@@ -28,16 +28,16 @@ def save_nph_stock_spot_data(date, before=True):
         if data is None or len(data.index) == 0:
             return
 
-        table_name = tbs.TABLE_CN_STOCK_SPOT['name']
+        table_name = instock.core.tablestructure.TABLE_CN_STOCK_SPOT['name']
         # 删除老数据。
-        if mdb.checkTableIsExist(table_name):
+        if instock.lib.database.checkTableIsExist(table_name):
             del_sql = f"DELETE FROM `{table_name}` where `date` = '{date}'"
-            mdb.executeSql(del_sql)
+            instock.lib.database.executeSql(del_sql)
             cols_type = None
         else:
-            cols_type = tbs.get_field_types(tbs.TABLE_CN_STOCK_SPOT['columns'])
+            cols_type = instock.core.tablestructure.get_field_types(instock.core.tablestructure.TABLE_CN_STOCK_SPOT['columns'])
 
-        mdb.insert_db_from_df(data, table_name, cols_type, False, "`date`,`code`")
+        instock.lib.database.insert_db_from_df(data, table_name, cols_type, False, "`date`,`code`")
 
     except Exception as e:
         logging.error(f"basic_data_daily_job.save_stock_spot_data处理异常：{e}")
@@ -49,27 +49,27 @@ def save_nph_etf_spot_data(date, before=True):
         return
     # 股票列表
     try:
-        data = stf.fetch_etfs(date)
+        data = instock.core.stockfetch.fetch_etfs(date)
         if data is None or len(data.index) == 0:
             return
 
-        table_name = tbs.TABLE_CN_ETF_SPOT['name']
+        table_name = instock.core.tablestructure.TABLE_CN_ETF_SPOT['name']
         # 删除老数据。
-        if mdb.checkTableIsExist(table_name):
+        if instock.lib.database.checkTableIsExist(table_name):
             del_sql = f"DELETE FROM `{table_name}` where `date` = '{date}'"
-            mdb.executeSql(del_sql)
+            instock.lib.database.executeSql(del_sql)
             cols_type = None
         else:
-            cols_type = tbs.get_field_types(tbs.TABLE_CN_ETF_SPOT['columns'])
+            cols_type = instock.core.tablestructure.get_field_types(instock.core.tablestructure.TABLE_CN_ETF_SPOT['columns'])
 
-        mdb.insert_db_from_df(data, table_name, cols_type, False, "`date`,`code`")
+        instock.lib.database.insert_db_from_df(data, table_name, cols_type, False, "`date`,`code`")
     except Exception as e:
         logging.error(f"basic_data_daily_job.save_nph_etf_spot_data处理异常：{e}")
 
 
 def main():
-    runt.run_with_args(save_nph_stock_spot_data)
-    runt.run_with_args(save_nph_etf_spot_data)
+    instock.lib.run_template.run_with_args(save_nph_stock_spot_data)
+    instock.lib.run_template.run_with_args(save_nph_etf_spot_data)
 
 
 # main函数入口
